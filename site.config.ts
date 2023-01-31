b833ed17fc5ad61797ccde5bc25d89efae078043
Zhs111 <124028452+Zhs111@users.noreply.github.com>
2023-01-31 15:04:01 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '62703b6cfa6c4052bf0dc101976d943e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '建筑2102班主页',
+  domain: 'https://www.notion.so/2102-62703b6cfa6c4052bf0dc101976d943e',
+  author: 'ZHS',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
